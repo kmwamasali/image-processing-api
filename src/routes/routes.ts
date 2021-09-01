0184f458620bacc75564d15b231bd810766760a7
kmwamasali <kmwamasali@gmail.com>
2021-09-01 14:53:50 +0300

    Feature add compression middleware

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,9 +1,33 @@
+import fs from 'fs';
 import express from 'express';
+import path from 'path';
+import sharp from 'sharp';
 
 const router = express.Router();
 
-router.get('/images', (req, res) => {
-  res.send('images url endpoint');
+const getImageFile = async (req: any, res: express.Response, next: express.NextFunction) => {
+  const { filename } = req.query;
+  const width = parseInt(req.query.width, 10);
+  const height = parseInt(req.query.height, 10);
+
+  const fullImageFilepath = path.join(__dirname, '../images/full', `${filename}.jpg`);
+  const thumbImageFilepath = path.join(__dirname, '../images/thumb', `${filename}.jpg`)
+
+  const imageBuffer = await sharp(fullImageFilepath)
+    .resize(width, height)
+    .toBuffer();
+
+  fs.writeFile(thumbImageFilepath, imageBuffer, (err) => {
+    if (err) console.log(err)
+    next();
+  });
+}
+
+router.get('/images', getImageFile, (req, res) => {
+  const { filename } = req.query;
+
+  const imageFilepath = path.join(__dirname, '../images/thumb', `${filename}.jpg`);
+  res.sendFile(imageFilepath);
 });
 
 export default router;
